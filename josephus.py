84532acbc50439383fcd3d7ebbd55203ea9ec80d
Abraham Binder <abebinder@gmail.com>
2021-03-12 01:42:24 -0800

    reformatted

--- josephus.py
+++ josephus.py
@@ -1,12 +1,14 @@
 def josephus(n):
-    return (n - biggest_power_of_two(n))*2 + 1
+    return (n - biggest_power_of_two(n)) * 2 + 1
+
 
 def biggest_power_of_two(n):
-    counter =0
-    while(n>0):
-        n//=2
-        counter+=1
-    return 2**(counter-1)
+    counter = 0
+    while (n > 1):
+        n //= 2
+        counter += 1
+    return 2 ** counter
+
 
 if __name__ == '__main__':
     print(josephus(41))
